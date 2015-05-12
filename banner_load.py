5413a8e7bcfe7050c2c336af7e1f81f1812b643f
Justin Littman <justinlittman@gwu.edu>
2015-05-12 11:15:38 -0400

    Switched to pipe delimited for banner data.

--- banner_load.py
+++ banner_load.py
@@ -50,14 +50,17 @@ pos_code_to_classes = {
     "30501": "Librarian",
 }
 
+#Register banner dialect
+csv.register_dialect("banner", delimiter="|")
+
 
 def get_non_faculty_gwids(data_dir, non_fac_limit=None):
     gwids = []
     with codecs.open(os.path.join(data_dir, "vivo_emplappt.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row in reader:
-            if row[2] in pos_code_to_classes:
-                gwids.append(row[0])
+            if row["POSITION_CLASS"] in pos_code_to_classes:
+                gwids.append(row["EMPLOYEEID"])
     demo_gwids = demographic_intersection(gwids, data_dir)
     if non_fac_limit and len(demo_gwids) > non_fac_limit:
         return demo_gwids[:non_fac_limit]
@@ -68,9 +71,9 @@ def get_non_faculty_gwids(data_dir, non_fac_limit=None):
 def get_faculty_gwids(data_dir, fac_limit=None):
     gwids = set()
     with codecs.open(os.path.join(data_dir, "vivo_acadappt.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row in reader:
-            gwids.add(row[0])
+            gwids.add(row["EMPLOYEEID"])
     demo_gwids = demographic_intersection(gwids, data_dir)
     if fac_limit and len(demo_gwids) > fac_limit:
         return demo_gwids[:fac_limit]
@@ -81,9 +84,9 @@ def get_faculty_gwids(data_dir, fac_limit=None):
 def demographic_intersection(gwids, data_dir):
     demo_gwids = set()
     with codecs.open(os.path.join(data_dir, "vivo_demographic.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row in reader:
-            demo_gwids.add(row[0])
+            demo_gwids.add(row["EMPLOYEEID"])
     return list(demo_gwids.intersection(gwids))
 
 
@@ -93,10 +96,10 @@ def print_position_code_to_name(data_dir):
     """
     positions = {}
     with codecs.open(os.path.join(data_dir, "vivo_emplappt.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row in reader:
-            pos_code = row[2]
-            pos_name = row[1]
+            pos_code = row["POSITION_CLASS"]
+            pos_name = row["JOB_TITLE"]
             if not pos_code in positions:
                 positions[pos_code] = []
             if not pos_name in positions[pos_code]:
@@ -118,23 +121,23 @@ def load_demographic(data_dir, limit=None, fac_limit=None, non_fac_limit=None):
     faculty_gwids = get_faculty_gwids(data_dir, fac_limit=fac_limit)
 
     with codecs.open(os.path.join(data_dir, "vivo_demographic.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         p_count = 0
         for row in reader:
-            gw_id = row[0]
+            gw_id = row["EMPLOYEEID"]
             if gw_id in faculty_gwids or gw_id in non_faculty_gwids:
                 p = Person(gw_id)
-                p.first_name = row[1] if row[1] else None
-                p.middle_name = row[2] if row[2] else None
-                p.last_name = row[3] if row[3] else None
-                p.address1 = row[4] if row[4] else None
-                p.address2 = row[5] if row[5] else None
-                p.address3 = row[6] if row[6] else None
-                p.city = row[7] if row[7] else None
-                p.state = row[8] if row[8] else None
-                p.zip = row[10] if row[10] else None
-                p.email = row[11] if row[11] else None
-                p.phone = row[13] if row[13] else None
+                p.first_name = row["FIRST_NAME"] if row["FIRST_NAME"] else None
+                p.middle_name = row["MIDDLE_NAME"] if row["MIDDLE_NAME"] else None
+                p.last_name = row["LAST_NAME"] if row["LAST_NAME"] else None
+                p.address1 = row["ADDRESS_LINE1"] if row["ADDRESS_LINE1"] else None
+                p.address2 = row["ADDRESS_LINE2"] if row["ADDRESS_LINE2"] else None
+                p.address3 = row["ADDRESS_LINE3"] if row["ADDRESS_LINE3"] else None
+                p.city = row["CITY"] if row["CITY"] else None
+                p.state = row["STATE"] if row["STATE"] else None
+                p.zip = row["ZIP"] if row["ZIP"] else None
+                p.email = row["EMAIL"] if row["EMAIL"] else None
+                p.phone = row["PHONE"] if row["PHONE"] else None
 
                 g += p.to_graph()
 
@@ -158,15 +161,15 @@ def load_emplappt(data_dir, limit=None, non_fac_limit=None):
     non_faculty_gwids = get_non_faculty_gwids(data_dir, non_fac_limit=non_fac_limit)
 
     with codecs.open(os.path.join(data_dir, "vivo_emplappt.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         p_count = 0
         for row in reader:
-            gw_id = row[0]
-            pos_cd = row[2]
+            gw_id = row["EMPLOYEEID"]
+            pos_cd = row["POSITION_CLASS"]
             if gw_id in non_faculty_gwids and pos_cd in pos_code_to_classes:
                 nf = NonFaculty(Person(gw_id), pos_code_to_classes[pos_cd])
-                nf.title = row[1]
-                nf.home_organization = Organization(row[3])
+                nf.title = row["JOB_TITLE"]
+                nf.home_organization = Organization(row["HOME_ORG_CODE"])
                 g += nf.to_graph()
 
                 p_count += 1
@@ -187,19 +190,19 @@ def load_orgn(data_dir, limit=None):
     #Only load organizations that have entries in emplappt
     org_cds = set()
     with codecs.open(os.path.join(data_dir, "vivo_emplappt.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row in reader:
-            if row[2] in pos_code_to_classes:
-                org_cds.add(row[3])
+            if row["POSITION_CLASS"] in pos_code_to_classes:
+                org_cds.add(row["HOME_ORG_CODE"])
 
     with codecs.open(os.path.join(data_dir, "vivo_orgn.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         o_count = 0
         for row in reader:
-            org_cd = row[0]
+            org_cd = row["ORG_CODE"]
             if org_cd in org_cds:
-                o = Organization(row[0])
-                o.name = row[1]
+                o = Organization(org_cd)
+                o.name = row["ORG_TITLE"]
                 g += o.to_graph()
 
                 o_count += 1
@@ -216,9 +219,9 @@ def load_college(data_dir, limit=None):
     college_cds = set()
     #Only load colleges that have entries in acadappt
     with codecs.open(os.path.join(data_dir, "vivo_acadappt.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row in reader:
-            college_cds.add(row[1])
+            college_cds.add(row["COLLEGE"])
 
     #Remove "No College Designated"
     college_cds.remove("00")
@@ -228,13 +231,13 @@ def load_college(data_dir, limit=None):
 
     #"01","Columbian Col & Grad School"
     with open(os.path.join(data_dir, "vivo_college.txt"), 'rb') as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         o_count = 0
         for row in reader:
-            college_cd = row[0]
+            college_cd = row["COLLEGE_CD"]
             if college_cd in college_cds:
                 o = Organization(college_cd, organization_type="College")
-                o.name = row[1]
+                o.name = row["COLLEGE"]
                 g += o.to_graph()
                 o_count += 1
                 if limit and o_count >= limit:
@@ -254,19 +257,19 @@ def load_depart(data_dir, limit=None):
     #Read acadappt to get map of department to college
     department_to_college_dict = {}
     with codecs.open(os.path.join(data_dir, "vivo_acadappt.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row in reader:
-            department_to_college_dict[row[2]] = row[1]
+            department_to_college_dict[row["DEPARTMENT"]] = row["COLLEGE"]
 
     #"HKLS","Human Kinetics&Leisure Studies"
     with codecs.open(os.path.join(data_dir, "vivo_depart.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         o_count = 0
         for row in reader:
-            dept_cd = row[0]
+            dept_cd = row["DEPARTMENT_CD"]
             if dept_cd not in ("0000",):
                 o = Organization(dept_cd, organization_type="Department")
-                o.name = row[1]
+                o.name = row["DEPARTMENT"]
                 o.part_of = Organization(department_to_college_dict.get(dept_cd))
                 g += o.to_graph()
                 o_count += 1
@@ -290,14 +293,14 @@ def load_acadappt(data_dir, limit=None, load_appt=True, fac_limit=None):
     faculty_gwids = get_faculty_gwids(data_dir, fac_limit=fac_limit)
 
     with codecs.open(os.path.join(data_dir, "vivo_acadappt.txt"), 'r', encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row_num, row in enumerate(reader):
-            gw_id = row[0]
+            gw_id = row["EMPLOYEEID"]
             if gw_id in faculty_gwids:
                 f = Faculty(Person(gw_id), load_appt=load_appt)
-                f.department = Organization(row[2])
-                f.title = row[4]
-                f.start_term = row[5]
+                f.department = Organization(row["DEPARTMENT"])
+                f.title = row["POSITION_TITLE"]
+                f.start_term = row["START_TERM_CODE"]
                 g += f.to_graph()
 
                 if limit and row_num >= limit-1:
@@ -320,12 +323,12 @@ def load_courses(data_dir, limit=None, fac_limit=None):
 
     #This file is supposed to be utf-8, but is not valid.
     with codecs.open(os.path.join(data_dir, "vivo_courses.txt"), 'r', encoding="utf-8", errors="ignore") as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.DictReader(csv_file, dialect="banner")
         for row_num, row in enumerate(reader):
-            gw_id = row[0]
+            gw_id = row["EMPLOYEEID"]
             if gw_id in faculty_gwids:
-                c = Course(Person(gw_id), row[1], row[2], row[3])
-                c.course_title = row[4]
+                c = Course(Person(gw_id), row["COURSE_NBR"], row["SUBJECT"], row["TERM_CODE"])
+                c.course_title = row["COURSE_TITLE"]
                 g += c.to_graph()
 
                 if limit and row_num >= limit-1:
