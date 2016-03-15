4d876b6c059f4a0e42c6a924b9c4fd051e8f9a3f
Justin Littman <justinlittman@gwu.edu>
2016-03-15 14:18:06 -0400

    Switched name order to testing purposes.

--- loader/banner_entity.py
+++ loader/banner_entity.py
@@ -27,7 +27,10 @@ class Person():
         #Create an RDFLib Graph
         g = Graph()
 
-        full_name = join_if_not_empty((self.first_name, self.middle_name, self.last_name))
+        # Switched for testing sorting
+        # full_name = join_if_not_empty((self.first_name, self.middle_name, self.last_name))
+        full_name = join_if_not_empty((self.last_name, self.first_name, self.middle_name))
+
 
         ##Person
         g.add((self.uri, RDFS.label, Literal(full_name)))
